180b2b36b9ed909679a7ea72b5ea9dc0a19246f5
Subhash Chandra <subhash@192.168.1.8>
2026-06-24 10:20:50 +0530

    Initial commit

---
+++ src/test/java/com/subhash/messaging/messaging_service/MessaginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhash.messaging.messaging_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessaginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
